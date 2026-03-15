2b1cf84d72917a099849c781bf3e19a2347bc93d
Dillow <madushanka20021224@gmail.com>
2026-03-15 18:55:49 +0530

    create product modle

---
+++ backend/models/Product.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+const productSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  description: { type: String, required: true },
+  price: { type: Number, required: true },
+  image: { type: String, required: true },
+  category: { type: String, enum: ["Men", "Women", "Kids"], required: true },
+  sizes: [{ type: String, enum: ["S", "M", "L", "XL"] }],
+  createdAt: { type: Date, default: Date.now },
+});
+
+module.exports = mongoose.model("Product", productSchema);
