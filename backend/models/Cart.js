422d91b19e380ff9e48b2a337f37e2de737fc97d
Dillow <madushanka20021224@gmail.com>
2026-03-15 19:14:17 +0530

    cart model created

---
+++ backend/models/Cart.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+const cartItemSchema = new mongoose.Schema({
+  product: { type: mongoose.Schema.Types.ObjectId, ref: "Product", required: true },
+  size: { type: String, enum: ["S","M","L","XL"], required: true },
+  quantity: { type: Number, default: 1 },
+});
+
+const cartSchema = new mongoose.Schema({
+  user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: false }, // guest cart allowed
+  items: [cartItemSchema],
+  createdAt: { type: Date, default: Date.now },
+});
+
+module.exports = mongoose.model("Cart", cartSchema);
