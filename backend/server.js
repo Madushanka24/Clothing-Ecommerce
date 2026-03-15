3ac651900267b1000565107d3796bf631fb5ade4
Dillow <madushanka20021224@gmail.com>
2026-03-15 18:46:49 +0530

    postmen api checked

--- backend/server.js
+++ backend/server.js
@@ -20,3 +20,10 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+app.use(express.json());
+
+const authRoutes = require("./routes/authRoutes");
+
+// Routes
+app.use("/api/auth", authRoutes);
