a1f4d73107691d48911bea6ef1602d2f7200a928
Dillow <madushanka20021224@gmail.com>
2026-03-16 17:49:14 +0530

    change everything

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -4,13 +4,14 @@ const API = axios.create({
   baseURL: "http://localhost:5000/api",
 });
 
-// attach token if exists
-API.interceptors.request.use((config) => {
+API.interceptors.request.use((req) => {
   const token = localStorage.getItem("token");
+
   if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
+    req.headers.Authorization = `Bearer ${token}`;
   }
-  return config;
+
+  return req;
 });
 
 export default API;
