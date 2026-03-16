3c89f23dbf8d83ef72130606c878ce15fad9f287
Dillow <madushanka20021224@gmail.com>
2026-03-16 13:05:24 +0530

    middleweare update

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -1,33 +1,28 @@
 const jwt = require("jsonwebtoken");
 const User = require("../models/User");
 
-const protect = async (req, res, next) => {
+exports.protect = async (req, res, next) => {
   let token;
 
   if (
     req.headers.authorization &&
     req.headers.authorization.startsWith("Bearer")
   ) {
-    try {
-      // Get token from header
-      token = req.headers.authorization.split(" ")[1];
-
-      // Verify token
-      const decoded = jwt.verify(token, process.env.JWT_SECRET);
-
-      // Get user from DB without password
-      req.user = await User.findById(decoded.id).select("-password");
-
-      next();
-    } catch (err) {
-      console.error(err);
-      res.status(401).json({ message: "Not authorized" });
-    }
+    token = req.headers.authorization.split(" ")[1];
   }
 
   if (!token) {
     return res.status(401).json({ message: "Not authorized, no token" });
   }
-};
 
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    req.user = await User.findById(decoded.id).select("-password");
+    next();
+  } catch (err) {
+    console.error(err);
+    res.status(401).json({ message: "Not authorized, token failed" });
+  }
+};
 module.exports = { protect };
