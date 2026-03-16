419d47eb759d32f84d1b8d474f8007903ce30b91
Dillow <madushanka20021224@gmail.com>
2026-03-16 11:51:58 +0530

    fixed errors

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,37 @@
-import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import ProductDetails from "./pages/ProductDetails";
+import Cart from "./pages/Cart";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Checkout from "./pages/Checkout";
 
 function App() {
+
   return (
-    <div className="container mt-5">
-      <h1>Clothing Store</h1>
-      <p>Welcome to my MERN E-commerce App</p>
-    </div>
+    <Router>
+
+      <Navbar />
+
+      <Routes>
+
+        <Route path="/" element={<Home />} />
+
+        <Route path="/product/:id" element={<ProductDetails />} />
+
+        <Route path="/cart" element={<Cart />} />
+
+        <Route path="/login" element={<Login />} />
+
+        <Route path="/register" element={<Register />} />
+
+        <Route path="/checkout" element={<Checkout />} />
+
+      </Routes>
+
+    </Router>
   );
 }
 
--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,8 +1,10 @@
 import { Link } from "react-router-dom";
 
 function Navbar() {
+
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+
       <div className="container">
 
         <Link className="navbar-brand" to="/">
@@ -10,13 +12,27 @@ function Navbar() {
         </Link>
 
         <div className="navbar-nav">
-          <Link className="nav-link" to="/">Home</Link>
-          <Link className="nav-link" to="/cart">Cart</Link>
-          <Link className="nav-link" to="/login">Login</Link>
-          <Link className="nav-link" to="/register">Register</Link>
+
+          <Link className="nav-link" to="/">
+            Home
+          </Link>
+
+          <Link className="nav-link" to="/cart">
+            Cart
+          </Link>
+
+          <Link className="nav-link" to="/login">
+            Login
+          </Link>
+
+          <Link className="nav-link" to="/register">
+            Register
+          </Link>
+
         </div>
 
       </div>
+
     </nav>
   );
 }
