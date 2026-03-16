87b8fd3144901db731eb99b50cfb486a4e1c3147
Dillow <madushanka20021224@gmail.com>
2026-03-16 11:13:48 +0530

    app.js

--- frontend/App.js
+++ frontend/App.js
@@ -1,24 +1,36 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import ProductDetails from "./pages/ProductDetails";
 import Cart from "./pages/Cart";
-import Checkout from "./pages/Checkout";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
-import Navbar from "./components/Navbar";
+import Checkout from "./pages/Checkout";
 
 function App() {
+
   return (
     <Router>
+
       <Navbar />
+
       <Routes>
+
         <Route path="/" element={<Home />} />
+
         <Route path="/product/:id" element={<ProductDetails />} />
+
         <Route path="/cart" element={<Cart />} />
-        <Route path="/checkout" element={<Checkout />} />
+
         <Route path="/login" element={<Login />} />
+
         <Route path="/register" element={<Register />} />
+
+        <Route path="/checkout" element={<Checkout />} />
+
       </Routes>
+
     </Router>
   );
 }
